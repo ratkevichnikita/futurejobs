414d7770ee1b0827aeed6fe6dce7b1fb7e2e7ee3
nikita <nikita.ratkevich91@mail.ru>
2024-09-04 22:21:26 +0200

    start project

---
+++ src/lib/fonts.ts
@@ -0,0 +1,8 @@
+import { Roboto } from "next/font/google";
+
+export const fontRoboto = Roboto({
+  weight: ["400","700", "900"],
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
